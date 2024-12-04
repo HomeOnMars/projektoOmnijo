d5ddc4e9682d8a13f02e6f336d75d7052f6e19ba
HomeOnMars <159873500+HomeOnMars@users.noreply.github.com>
2024-12-04 18:04:04 +1100

    Updated radio/reformatigi.py to include auto-renaming function

--- radio/reformatigi.py
+++ radio/reformatigi.py
@@ -11,6 +11,9 @@ Require ffmpeg and libvorbis (install with apt - as I only tested this on Ubuntu
 Only tested on WSL (Windows Subsystem for Linux).
 Most likely will NOT work in Windows directly.
 
+Warning: if the song file name ended with a number, the script may not treat it correctly.
+Rename it like: from '42.ogg' to '01_42.ogg' as a fix.
+
 
 Author: HomeOnMars
 
@@ -110,12 +113,51 @@ def write_json(
         if verbose: print(f" Done.")
     return
 
+def name_func_default(no: int, old_filename: str) -> str:
+    """Default function for generating new names from old ones"""
+
+    # new_fn = f"{no:03}_{'_'.join(old_filename.split('_')[1:])}".translate(None, ' \'\\/:*?"<>|')
+    new_fn = old_filename
+    fns: list = new_fn.split('_')
+    if fns[0] and fns[0][-1] in set('0123456789'):    # first word: my no of the file
+        fns = fns[1:]
+    new_fn = '_'.join(fns)
+
+    fns: list = new_fn.split(' ')
+    if fns[0] and fns[0][-1] in set('0123456789'):    # first word: album no of the file
+        fns = fns[1:]
+    # titlize and remove labels
+    fns = [
+        fn.title() for fn in fns
+        if fn.title() not in {
+            '(Acoustic)',
+            '[Acoustic]',
+        }
+    ]
+    # remove '(feat. xxx)' info
+    new_fns = []
+    par_lvl : int = 0    # par means parenthesis
+    for fn in fns:
+        if fn.startswith('(Feat'):
+            par_lvl += 1
+        if not par_lvl:
+            new_fns.append(fn)
+        if par_lvl and fn.endswith(')'):
+            par_lvl -= 1
+    new_fn = ''.join(new_fns)    # throw away spaces
+    
+    # normalize - throw away any char unsupported by windows file explorer
+    new_fn = f"{no:03}_{new_fn}".translate({c: None for c in ' \'\\/:*?"<>|'})
+    return new_fn
+
+
 
 # main function
 
 def normalize_csl2_music_files(
     name_func = lambda no, old_filename: f"{old_filename}", #f"",
     tbc_ext_set: set[str] = {'.m4a'},    # tbc = to be converted; ext = file name extension
+    overwrite_move: None|bool = None,    # if None, will raise exception if file already exists
     overwrite_ogg : None|bool = None,    # if None, will raise exception if file already exists
     overwrite_json: None|bool = None,
     do_sort_fname : bool = True,
@@ -135,7 +177,7 @@ def normalize_csl2_music_files(
     """
 
     # init
-
+    all_ext_set = tbc_ext_set.copy(); all_ext_set.add('.ogg')
     filenames_raw_list: list[str] = os.listdir('.')
     filenames_dict: dict[str: set[str]] = {}
     for filename in filenames_raw_list:
@@ -146,7 +188,22 @@ def normalize_csl2_music_files(
             filenames_dict[fn].add(ext)
 
 
-    # sort
+    # sort file names
+    if do_sort_fname:
+        filenames_old_list : list = [
+            fn for fn, exts in filenames_dict.items()
+            if all_ext_set.intersection(exts)    # select music files only
+        ]
+        filenames_old_list.sort(key = lambda txt: txt.split('_')[0])
+        for i, fn in enumerate(filenames_old_list):
+            new_fn = name_func(i, fn)
+            filenames_dict[new_fn] = filenames_dict.pop(fn)
+            for ext in filenames_dict[new_fn]:
+                mv(
+                    f'{fn}{ext}', f'{new_fn}{ext}',
+                    overwrite=overwrite_move, dry_run=dry_run, verbose=verbose)
+
+    
 
     # convert to ogg and add json files
     if do_convert_ogg:
@@ -187,9 +244,10 @@ def normalize_csl2_music_files(
 
 if __name__ == '__main__':
     normalize_csl2_music_files(
-        # name_func = lambda no, old_filename: f"{old_filename}",
+        name_func = lambda no, old_filename: f"M{name_func_default(no, old_filename)}",
         overwrite_ogg = False,
         overwrite_json= False,
         dry_run  = False,
         verbose  = True,
     )
+"".translate()
