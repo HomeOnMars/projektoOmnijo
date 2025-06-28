f77a470859e2150cebbeb9474910e1553f8d124f
HomeOnMars <159873500+HomeOnMars@users.noreply.github.com>
2025-06-28 10:58:23 +1000

    Rename SkyvePlayset normalization script

--- OmniCentro/Sxangxonoto/gxisdatigi.sh
+++ OmniCentro/Sxangxonoto/gxisdatigi.sh
@@ -1,30 +1,30 @@
 #!/bin/bash
 
 # Changelogs
-echo
+echo; echo
 pwd -P
-echo
+echo; echo
 cp -v ../Data/.markdownlint.json   ./
 cp -v ../Data/Changelog.md         Sxangxonoto.md
 cp -v ../Data/Changelog_tags.md    Sxangxonoto_bib.md
 cp -v ../Data/Changelog_public.md  Sxangxonoto_pub.md
 
 # Playsets
-echo
+echo; echo
 cd ../../SkyvePlayset/
 pwd -P
 echo
-mv -vu ../CSL2_SavesDir/ModsData/Skyve/Playsets/Shared/OmniCentro*.json ./temp.OmniCentro.json
+python3 skyveplayset.py
 
 # ModsSettings
-echo
+echo; echo
 cd OmniCentro_ModsSettings/
 pwd -P
-echo
+echo; echo
 cp -v ../../CSL2_SavesDir/RealLife.coc ./
 cp -vr ../../CSL2_SavesDir/ModsSettings/Time2Work ModsSettings/
 
 # Reminders
-echo
+echo; echo
 echo "***REMEMBER TO UPDATE THE PLAYSET INFO***"
 echo
